9d24801862bc619f0f09ba54f5e02d822b7aa5d2
root <root@inst-zoomcamp.southamerica-west1-b.c.flawless-snow-432721-k3.internal>
2024-08-29 23:55:13 +0000

    initial commit

---
+++ 01-docker-terraform/2_docker_sql/pipeline.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+import sys
+
+print(sys.argv)
+
+print(sys.argv[1])
+
+print("job finished :)")
+
+#gcloud compute scp --recurse "c:/Users/imati/Documents/zoomcamp/01-docker-terraform/2_docker_sql/ny_taxi_postgres_data/*" inst-zoomcamp:~/zoomcamp/01-docker-terraform/2_docker_sql/ny_taxi_postgres_data
